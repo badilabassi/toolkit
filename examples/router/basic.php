12c09587c8695d7ee8c954f7ef3115c0f8a7d3b5
Bastian Allgeier <bastian.allgeier@gmail.com>
2013-07-18 16:01:40 +0200

    new router filter

--- examples/router/basic.php
+++ examples/router/basic.php
@@ -9,16 +9,16 @@ $currentURL = 'http://mydomain.com/api/users/getkirby/posts';
 
 // register a new GET route 
 router::get('api/users/(:any)', function($username) {
-  echo 'user profile for: ' . $username;
+  dump('user profile for: ' . $username);
 });
 
 // register another GET route
 router::get('api/users/(:any)/posts', function($username) {
-  echo 'posts for user: ' . $username . '...';
+  dump('posts for user: ' . $username . '...');
 });
 
 // check for a matching route 
 if($route = router::match($currentURL)) {
   // call the route action and pass all variables from the url
-  call_user_func_array($route->action(), $route->options());
+  router::call($route);
 }
--- lib/router.php
+++ lib/router.php
@@ -50,12 +50,15 @@ class Router {
     '/(:all?)'   => '(?:/(.*)',
   );
 
+  // additional events, which can be triggered by routes
+  static protected $filters = array();
+
   /**
    * Resets all registered routes
    */
   static public function reset() {
-    self::$route  = null;
-    self::$routes = array(
+    static::$route  = null;
+    static::$routes = array(
       'GET'    => array(),
       'POST'   => array(),
       'PUT'    => array(),
@@ -69,7 +72,7 @@ class Router {
    * @return mixed
    */
   static public function route() {
-    return self::$route;
+    return static::$route;
   }
 
   /**
@@ -78,7 +81,7 @@ class Router {
    * @return array
    */
   static public function options() {
-    if($route = self::route()) return $route->options();
+    if($route = static::route()) return $route->options();
   }
 
   /**
@@ -87,27 +90,47 @@ class Router {
    * @param string|array $methods GET, POST, PUT, DELETE
    * @param string $uri The url pattern, which should be matched
    * @param mixed $action An array of parameters for this route
+   * @param mixed $filters Either a single name of a filter or an array of filter names
    */
-  static public function register($methods, $pattern, $action) {
+  static public function register($methods, $pattern, $action, $filters = null) {
     foreach((array)$methods as $method) {
-      self::$routes[$method][ltrim($pattern, '/')] = $action;
+      static::$routes[$method][ltrim($pattern, '/')] = array('action' => $action, 'filters' => $filters);
     }
   }
 
-  static public function get($pattern, $action) {
-    self::register('GET', $pattern, $action);
+  static public function get($pattern, $action, $filters = null) {
+    static::register('GET', $pattern, $action, $filters);
   }
 
-  static public function post($pattern, $action) {
-    self::register('POST', $pattern, $action);
+  static public function post($pattern, $action, $filters = null) {
+    static::register('POST', $pattern, $action, $filters);
   }
 
-  static public function put($pattern, $action) {
-    self::register('PUT', $pattern, $action);
+  static public function put($pattern, $action, $filters = null) {
+    static::register('PUT', $pattern, $action, $filters);
   }
 
-  static public function delete($pattern, $action) {
-    self::register('DELETE', $pattern, $action);
+  static public function delete($pattern, $action, $filters = null) {
+    static::register('DELETE', $pattern, $action, $filters);
+  }
+
+  /**
+   * Add a new router filter
+   * 
+   * @param string $name A simple name for the filter, which can be used by routes later
+   * @param closure $function A filter function, which should be called before routes 
+   */
+  static public function filter($name, $function) {
+    static::$filters[$name] = $function;
+  }
+
+  /**
+   * Return all registered filters
+   * 
+   * @return array
+   */
+  static public function filters() {
+    return static::$filters;
   }
 
   /**
@@ -117,7 +140,7 @@ class Router {
    * @return array
    */
   static public function routes($method = null) {
-    return is_null($method) ? self::$routes : self::$routes[$method];
+    return is_null($method) ? static::$routes : static::$routes[$method];
   }
 
   /**
@@ -130,16 +153,20 @@ class Router {
 
     $url    = is_null($url) ? uri::current()->path()->toString() : trim(url::path($url), '/');
     $method = r::method();
-    $routes = self::$routes[$method];
+    $routes = static::$routes[$method];
 
     // empty urls should never happen
     if(empty($url)) $url = '/';
 
-    foreach($routes as $pattern => $action) {
+    foreach($routes as $pattern => $route) {
+
+      $action  = $route['action'];
+      $filters = $route['filters']; 
 
       // handle exact matches
       if($pattern == $url) {
-        return self::$route = new Route($method, $pattern, $action, array());
+        static::filterer($filters);
+        return static::$route = new Route($method, $pattern, $action, array());
       }
 
       // We only need to check routes with regular expression since all others
@@ -147,17 +174,43 @@ class Router {
       // we just did before we started searching.
       if(!str::contains($pattern, '(')) continue;
       
-      $preg = '#^'. self::wildcards($pattern) . '$#u';
+      $preg = '#^'. static::wildcards($pattern) . '$#u';
 
       // If we get a match we'll return the route and slice off the first
       // parameter match, as preg_match sets the first array item to the
       // full-text match of the pattern.
       if(preg_match($preg, $url, $parameters)) {
-        return self::$route = new Route($method, $pattern, $action, array_slice($parameters, 1));
+        static::filterer($filters);
+        return static::$route = new Route($method, $pattern, $action, array_slice($parameters, 1));
       }    
     
     }
   
+  }
+
+  /**
+   * Call all matching filters
+   * 
+   * @param mixed $filters
+   */
+  static protected function filterer($filters) {
+    foreach((array)$filters as $filter) {
+      if(array_key_exists($filter, static::$filters) and is_callable(static::$filters[$filter])) {
+        call_user_func(static::$filters[$filter]);
+      }
+    }    
+  }
+
+  /**
+   * Call the action of a route if callable
+   * 
+   * @param object $route;
+   * @return mixed
+   */
+  static public function call($route) {
+    if(is_callable($route->action())) {
+      return call_user_func_array($route->action(), $route->options());
+    }
   }
 
   /**
@@ -168,8 +221,8 @@ class Router {
    */
   static protected function wildcards($pattern) {
       
-    $search  = array_keys(self::$optional);
-    $replace = array_values(self::$optional);
+    $search  = array_keys(static::$optional);
+    $replace = array_values(static::$optional);
 
     // For optional parameters, first translate the wildcards to their
     // regex equivalent, sans the ")?" ending. We'll add the endings
@@ -178,7 +231,7 @@ class Router {
 
     if($count > 0) $pattern .= str_repeat(')?', $count);
     
-    return strtr($pattern, self::$patterns);
+    return strtr($pattern, static::$patterns);
   
   }
 
